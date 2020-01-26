450141723c139a5b45c4b8ed280f24df70881bdb
Emmanuel <emmy0021@tamu.edu>
2020-01-25 19:04:27 -0600

    Working on firebase

--- location.js
+++ location.js
@@ -4,7 +4,10 @@ var city = '';
 var lon = '';
 var lat = '';
 
-console.log("working");
+var weather;
+var api = 'https://api.openweathermap.org/data/2.5/weather?';
+var apiKey = '&APPID=8697cfa385dec17d993f302610135c81';
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -32,7 +35,52 @@ function getLocation() {
 
 function outputCity() {
     document.getElementById("inputtext").value = city;
-    document.getElementById("lon").innerText = "longitide:" + lon;
-    document.getElementById("lat").innerText = "latitude:" + lat;
+    document.getElementById("lon").innerText = lon;
+    document.getElementById("lat").innerText = lat;
 
 }
+
+///weather
+function setup() {
+	createCanvas(400, 200);
+	var button = select('#submit');
+    button.mousePressed(weatherAsk);
+    button.mousePressed(writeData);
+}
+
+function weatherAsk() {
+    var latitude = 'lat=' + lat;
+
+	var longitude = '&lon=' + lon;
+	var url = api + latitude + longitude + apiKey;
+	
+	loadJSON(url, gotData);
+}
+function gotData(data) {
+    weather = data;
+    console.log(weather.main.temp);
+}
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCp__3sbc1Bj2B0h7Owyn_6KYH79XqkzC0",
+    authDomain: "tamuhack2020-69538.firebaseapp.com",
+    databaseURL: "https://tamuhack2020-69538.firebaseio.com",
+    projectId: "tamuhack2020-69538",
+    storageBucket: "tamuhack2020-69538.appspot.com",
+    messagingSenderId: "489082793913",
+    appId: "1:489082793913:web:918e698329f4482a1f1068",
+    measurementId: "G-SBWE9WF6DT"
+  };
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  function writeData(){
+      firebase.database().ref("coord").set({
+          Latitude: lat,
+          Longitude: lon,
+          Temperature: temp
+      })
+  }
